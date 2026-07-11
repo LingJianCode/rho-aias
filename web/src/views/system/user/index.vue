4f68effa088b612aeaf06d84c783086109ed9629
lingjiancode <mXf30fHJLqEEXGbzdqLYvD+lingjiancode@noreply.cnb.cool>
2026-07-11 13:56:42 +0800

    fix: 去掉未实现功能

--- web/src/views/system/user/index.vue
+++ web/src/views/system/user/index.vue
@@ -28,8 +28,8 @@
           <template #default="{ row }">
             <el-button link @click="handleEdit(row)">编辑</el-button>
             <el-button type="danger" link @click="handleDelete(row)">删除</el-button>
-          </template> -->
-        </el-table-column>
+          </template> 
+        </el-table-column> -->
       </el-table>
 
       <!-- 分页 -->
